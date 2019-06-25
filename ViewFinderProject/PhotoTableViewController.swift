57680d05b399a4f48d1c2574e29feba64791a15c
Apple <apple@Apples-MacBook-Pro-4.local>
2019-06-25 14:01:21 -0400

    Complete Iteration 4

--- ViewFinderProject/PhotoTableViewController.swift
+++ ViewFinderProject/PhotoTableViewController.swift
@@ -56,6 +56,24 @@ class PhotoTableViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+   
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+              if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let photoToDelete = photos[indexPath.row]
+                context.delete(photoToDelete)
+
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos()
+                
+                }
+            }
+        }
  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
@@ -65,17 +83,13 @@ class PhotoTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "moveToDetail" {
-            // need to get access to View Controller we want to move to
         }
-        if let photoDetailView = segue.destination as? PhotoDetailViewController {
-            
-            if let photoToSend = sender as? Photos {
+            if let photoDetailView = segue.destination as? PhotoDetailViewController {
+ 
+                if let photoToSend = sender as? Photos {
                 photoDetailView.photo = photoToSend
+                
             }
-
-            
         }
     }
-
-
 }
